759927d580ab4a14cd701db79a6e5283901b7726
BerivanDuranFHFrankfurt <149338780+BerivanDuranFHFrankfurt@users.noreply.github.com>
2023-10-30 22:05:09 +0100

    Add files via upload

---
+++ Quiz/geography.py
@@ -0,0 +1,57 @@
+# Fragen zur Geographie
+geography_lowlevel = [
+    ("Welches gehört zu den fünf Weltmeere?", "Der Pazifik", "Das schwarze Meer", "Der Golf Meer", "Die Nordsee", "Der Pazifik","Kein Hinweis"),
+    ("Wie heißt der Fluss, \nder durch den brasilianischen Regenwald fließt?",  "Rio Japurá", "Der Amazonas","Nil", "Paraná", "Der Amazonas","Kein Hinweis"),
+    ("Welches Land wird auch Niederlande genannt?", "Deutschland", "England","Holland", "Dänemark", "Holland","Kein Hinweis"),
+    ("Welcher der folgenden Kontinente ist der kälteste?", "Afrika", "Südamerika", "Antarktika", "Asien", "Antarktika","Kein Hinweis"),
+    ("Was ist die größte Wüste der Welt?", "Sahara", "Antarktische Wüste", "Gobi", "Kalaharis", "Antarktische Wüste","Kein Hinweis"),
+    ("Welche Insel ist für ihre einzigartige Tierwelt,\n einschließlich der Riesenschildkröten, berühmt?", "Madagaskar", "Island", "Hawaii", "Sri Lanka", "Madagaskar","Kein Hinweis"),
+    ("Welches Land hat die größte Bevölkerung der Welt?", "USA", "Russland",  "Indien","China", "China","Kein Hinweis"),
+    ("In welchem Land gibt es die \nmeisten aktiven Vulkane?", "Schweden", "Italien", "Türkei", "Indonesien", "Indonesien","Kein Hinweis"),
+    ("Was ist die größte Insel der Welt?", "Grönland", "Madagaskar", "Großbritannien", "Borneo", "Grönland","Kein Hinweis"),
+    ("Wo befindet sich der Eiffelturm?", "New York", "Paris", "Tokio", "Rom", "Paris","Kein Hinweis"),
+    ("Welches der folgenden Länder \nist am meisten von Erdbeben betroffen?", "Thailand", "Türkei", "Japan", "Marokko", "Japan","Kein Hinweis"),
+    ("In welchem Land befinden \nsich die Großen Pyramiden von Gizeh?", "Algerien", "Ägypten", "Marokko", "Türkei", "Ägypten","Kein Hinweis"),
+    ("Wie heißt der größte Fluss, \nder durch Deutschland fließt?",  "Main", "Neckar", "Oder", "Rhein", "Rhein","Kein Hinweis"),
+    ("Was ist die Hauptstadt Deutschlands?", "Berlin", "Köln", "Frankfurt am Main", "Bremen", "Berlin","Kein Hinweis"),
+    ("Wie viele Bundesländer hat Deutschland?",  "12", "18", "16","21", "16","Kein Hinweis"),
+]
+
+geography_midlevel = [
+    ("Was ist die Hauptstadt von Kanada?", "Ottawa", "Toronto", "Montreal", "Edmonton", "Ottawa", "Kein Hinweis"),
+    ("Wie heißt das kleinste Land der Welt?",  "Liechtenstein","Vatikanstadt", "Niue", "Monaco", "Vatikanstadt", "Kein Hinweis"),
+    ("Wie heißt der höchste Wasserfall der Welt?",  "Niagarafälle", "Shōmyō-Wasserfall","Salto Ángel", "Browne Falls", "Salto Ángel", "Kein Hinweis"),
+    ("Welches afrikanische Land \nhat die größte Bevölkerung?",  "Somalia", "Äthiopien", "Kenia","Nigeria", "Nigeria", "Kein Hinweis"),
+    ("Wie viele Zeitzonen hat Australien?", "Drei", "Eine", "Zwei", "Vier", "Drei", "Kein Hinweis"),
+    ("Was ist die offizielle Währung Saudi-Arabiens?", "Dirham", "Dinar", "Riyal", "Rial", "Riyal", "Kein Hinweis"),
+    ("Was ist der längste Fluss in Europa?",  "Rhein","Wolga", "Donau", "Weichsel", "Wolga", "Kein Hinweis"),
+    ("Welches Land in Europa ist ein Königreich?",  "Ungarn", "Bosnien \n& Herzegowina", "Finnland","Schweden", "Schweden", "Kein Hinweis"),
+    ("Was ist die Hauptstadt der UAE?", "Dubai", "Abu Dhabi", "Mekka", "Medina", "Abu Dhabi", "Kein Hinweis"),
+    ("Welches Land liegt über Brasilien?", "Venezuela", "Peru", "Paraguay", "Chile", "Venezuela", "Kein Hinweis"),
+    ("Aus wie vielen Staaten \nbestehen die Vereinigten Staaten?",  "52", "44", "50","34", "50", "Kein Hinweis"),
+    ("Wo befindet sich Babylon?", "Israel", "Palästina", "Syrien", "Irak", "Irak", "Kein Hinweis"),
+    ("Was ist ein Staat aus Ozeanen?", "Die Salomonen", "Thailand", "Vietnam", "Taiwan", "Die Salomonen", "Kein Hinweis"),
+    ("Welche Sprache spricht man in der Schweiz?", "Deutsch", "Französisch", "Italienisch", "Alle Drei", "Alle Drei", "Kein Hinweis"),
+    ("Durch welches Land verläuft der Äquator nicht?", "Somalia", "Indonesien", "Chile", "Brasilien", "Chile", "Kein Hinweis"),
+    ("Durch wie viele Bundesstaaten fließt der Mississippi?",  "22","31", "7", "9", "31", "Kein Hinweis")
+
+]
+
+geography_highlevel = [
+    ("Wie heißt der höchste Berg Kanadas?", "Mount Temple", "Mount Robson", "Mount Logan", "Mount Waddington", "Mount Logan", "Dieser Berg liegt im Yukon-Territorium."),
+    ("Was ist die größte Hauptstadt in Nordamerika?", "Ottawa", "Washington D.C",  "New York","Mexiko-Stadt", "Mexiko-Stadt", "Diese Hauptstadt ist auch die Hauptstadt eines Landes, das südlich der USA liegt."),
+    ("Welches der folgenden Länder \nhat die wenigsten Einwohner?", "Finnland", "Dänemark", "Schweiz", "Polen", "Finnland", "Dieses europäische Land hat eine Bevölkerungszahl von ca 5,5 Millionen."),
+    ("Zu welchem Land gehören die Kanarischen Inseln?",  "Portugal", "Spanien","Brasilien", "Venezuela", "Spanien", "Diese Inselgruppe liegt westlich von Afrika und gehört zu einem südeuropäischen Land."),
+    ("Wo steht das höchste \nvon Menschenhand geschaffene Bauwerk, \ndas jemals gebaut wurde?", "New York", "Dubai", "Shanghai", "Tokyo", "Dubai", "Dieses beeindruckende Bauwerk befindet sich in einem Emirat am Persischen Golf."),
+    ("Welche zwei Länder grenzen direkt \nnördlich an Ungarn?", "Slowakei & Ukraine", "Tschechien & Polen", "Slowenien & \nÖsterreich", "Rumänien & \nMoldawien", "Slowakei und Ukraine", "Eins dieser Länder grenzt an Belarus"),
+    ("Wie heißt der zweithöchste Berg der Welt?", "Makalu", "Mount Everest", "K2", "Cho Oyu", "K2", "Seinen Namen erhielt der Berg von dem britischen Vermessungsingenieur Thomas George Montgomerie, der 1856 die Gipfel im Karakorum aus größerer Entfernung kartierte."),
+    ("Wie heißt der erste Nationalpark der Welt?", "Grand-Teton", "Grand-Canyon", "Zion", "Yellowstone", "Yellowstone", "Dieser Nationalpark liegt hauptsächlich in den USA und wurde im Jahr 1872 gegründet."),
+    ("Wie viele Inseln bilden die Philippinen?", "50 Inseln", "1.000 Inseln", "7.641 Inseln", "15.000 Insel", "7.641 Inseln", " Die Anzahl der Inseln in den Philippinen liegt im vierstelligen Bereich."),
+    ("Welches Meer liegt östlich \nvon Florida und nördlich von Kuba?", "Golf von Mexiko", "Karibisches Meer", "Atlantischer Ozean", "Sargassosee", "Golf von Mexiko", "Dieses Meer erstreckt sich entlang der Ostküste der USA und grenzt an die Karibik."),
+    ("Wo steht das höchste \nvon Menschenhand geschaffene Bauwerk, \ndas jemals gebaut wurde?", "New York", "Dubai", "Shanghai", "Tokyo", "Dubai", "Dieses beeindruckende Bauwerk befindet sich in einem Emirat am Persischen Golf."),
+    ("Wie heißt die Hauptstadt Australiens?", "Sidney", "Brisbane", "Darwin", "Canberra", "Canberra", "Die Hauptstadt Australiens unterscheidet sich von den bekannten Großstädten des Landes"),
+    ("In welchem Land befindet sich der Mount Everest?", "Nepal", "USA", "Kanada", "Mongolei", "Nepal", "Dieser berühmte Berg liegt im Himalaya-Gebirge und liegt in Asien."),
+    ("Welche dieser Städte liegt am nördlichsten?", "Adelaide", "Perth", "Melbourne", "Brisbane", "Brisbane", "Diese Stadt liegt an der Küste und ist die Hauptstadt eines australischen Bundesstaates im Norden des Landes."),
+    ("Was ist die Landeshauptstadt von New York?",  "Portland","Albany", "New York City", "New Jersey", "Albany", "Diese Stadt liegt im Bundesstaat New York und ist nicht zu verwechseln mit der bekannteren Stadt im selben Bundesstaat."),
+    ("Welches Land teilt keine Grenze mit Russland?", "Mongolei", "Norwegen", "Usbekistan", "Lettland", "Usbekistan", "Dieses zentralasiatische Land liegt weit entfernt von Russland.")
+]
